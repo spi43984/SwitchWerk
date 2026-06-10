65310de8e1683d6a74809d636f93dc299095893e
spi <22529093+spi43984@users.noreply.github.com>
2026-06-10 16:17:15 +0200

    feat: add encrypted wifi credential storage

--- app/src/main/java/de/piecha/switchwerk/di/AppModule.kt
+++ app/src/main/java/de/piecha/switchwerk/di/AppModule.kt
@@ -4,7 +4,10 @@ import de.piecha.switchwerk.data.repository.DeviceRepository
 import de.piecha.switchwerk.data.repository.FakeDeviceRepository
 import de.piecha.switchwerk.data.repository.FakeWifiProfileRepository
 import de.piecha.switchwerk.data.repository.WifiProfileRepository
+import de.piecha.switchwerk.data.security.EncryptedWifiCredentialStore
+import de.piecha.switchwerk.data.security.WifiCredentialStore
 import de.piecha.switchwerk.viewmodel.MainViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
@@ -15,6 +18,12 @@ val appModule = module {
 
     single<WifiProfileRepository> {
         FakeWifiProfileRepository()
+    }
+
+    single<WifiCredentialStore> {
+        EncryptedWifiCredentialStore(
+            context = androidContext()
+        )
     }
 
     viewModel {
