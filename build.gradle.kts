154a18d5ff5f4efe0d4bacff51e147d195742ce4
spi <22529093+spi43984@users.noreply.github.com>
2026-07-04 15:59:17 +0000

    fix: address Dependabot build vulnerabilities

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    configurations.classpath {
+        resolutionStrategy {
+            force(
+                "org.bouncycastle:bcpkix-jdk18on:1.84",
+                "org.bouncycastle:bcprov-jdk18on:1.84",
+                "org.bouncycastle:bcutil-jdk18on:1.84",
+                "org.bitbucket.b_c:jose4j:0.9.6",
+                "org.jdom:jdom2:2.0.6.1",
+                "org.apache.commons:commons-lang3:3.18.0",
+            )
+        }
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
