865e6b283ebadb443de9ed68b1c5a6e040fe84f4
spi <22529093+spi43984@users.noreply.github.com>
2026-07-06 14:55:22 +0200

    chore: release 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "de.piecha.switchwerk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 809
-        versionName = "0.8.9"
+        versionCode = 900
+        versionName = "0.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
